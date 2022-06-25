2811faeb85602f5ff1a9018b71f3573bbd0261d4
AnatolSalau <salovanatoly5@mail.ru>
2022-06-25 22:28:01 +0300

    classwork 22

---
+++ lesson_22_classwork_servlets/src/main/java/HomeServlet.java
@@ -0,0 +1,14 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello from servlet");
+    }
+}
