0d3693ca324332817a0568c1fc407c1948f480dd
AnatolSalau <salovanatoly5@mail.ru>
2022-06-29 13:20:39 +0300

    Classwork lesson 23 JavaEE

--- lesson_23_classwork_javaee/src/main/java/listeners/MyRequestListener.java
+++ lesson_23_classwork_javaee/src/main/java/listeners/MyRequestListener.java
@@ -1,4 +1,20 @@
 package listeners;
 
-public class MyRequestListener {
+import jakarta.servlet.ServletRequestEvent;
+import jakarta.servlet.ServletRequestListener;
+
+public class MyRequestListener implements ServletRequestListener {
+    //Request уничтожается
+    //Приходят не requests а события!
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        ServletRequestListener.super.requestDestroyed(sre);
+    }
+
+    //Request инициализируется
+    //Приходят не requests а события!
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        ServletRequestListener.super.requestInitialized(sre);
+    }
 }
