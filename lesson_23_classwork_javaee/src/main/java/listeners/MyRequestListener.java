dd10d9a72d033c320a6119f5900776f02045850b
AnatolSalau <salovanatoly5@mail.ru>
2022-06-29 16:13:23 +0300

    Classwork lesson 23 JavaEE

--- lesson_23_classwork_javaee/src/main/java/listeners/MyRequestListener.java
+++ lesson_23_classwork_javaee/src/main/java/listeners/MyRequestListener.java
@@ -1,20 +1,29 @@
 package listeners;
 
+import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletRequestEvent;
 import jakarta.servlet.ServletRequestListener;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class MyRequestListener implements ServletRequestListener {
     //Request уничтожается
     //Приходят не requests а события!
     @Override
     public void requestDestroyed(ServletRequestEvent sre) {
-        ServletRequestListener.super.requestDestroyed(sre);
+        System.out.println("-------------REQUEST DESTROY--------------------");
     }
 
     //Request инициализируется
     //Приходят не requests а события!
     @Override
     public void requestInitialized(ServletRequestEvent sre) {
-        ServletRequestListener.super.requestInitialized(sre);
+        System.out.println("-------------REQUEST DESTROY--------------------");
+
+        //У пришедшего события можно получить ServletRequest
+        ServletRequest servletRequest = sre.getServletRequest();
+        //Преобразовать к httpRequest
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpServletRequest.getRequestURI();
+        System.out.println(requestURI);
     }
 }
