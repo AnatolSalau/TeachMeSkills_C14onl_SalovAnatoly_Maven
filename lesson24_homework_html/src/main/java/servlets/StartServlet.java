7779579a867abc9a6bcc1ebafd56a0776ff5eb41
AnatolSalau <salovanatoly5@mail.ru>
2022-07-20 14:01:26 +0300

    Lesson 24 homework html css

--- lesson24_homework_html/src/main/java/servlets/StartServlet.java
+++ lesson24_homework_html/src/main/java/servlets/StartServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import servlets.util.PrintOnPage;
-
+import jakarta.servlet.ServletRegistration.Dynamic;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
