dfe91a1ecdfffc2256167ff621221090190e8105
AnatolSalau <salovanatoly5@mail.ru>
2022-08-10 00:01:12 +0300

    SpringBoot Lesson1

---
+++ lesson34_classwork_boot/src/main/java/com/example/Lesson34ClassworkBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson34ClassworkBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson34ClassworkBootApplication.class, args);
+    }
+
+}
