c560cd64748b68daa0e95610b2a3b94bab6daeb7
AnatolSalau <salovanatoly5@mail.ru>
2022-09-29 15:01:37 +0300

    Lesson 38 classwork spring data jpa

---
+++ lesson38_classwork_spring_data_jpa/src/main/java/by/salov/Lesson38ClassworkSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package by.salov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson38ClassworkSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson38ClassworkSpringDataJpaApplication.class, args);
+    }
+
+}
