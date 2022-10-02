c935ad417a5149846e15deb7aa80f64097a4bf9e
AnatolSalau <salovanatoly5@mail.ru>
2022-10-02 20:20:25 +0300

    Lesson 44 Classwork SpringSecurity

---
+++ lesson44springsecurity/src/main/java/by/salov/Lesson44springsecurityApplication.java
@@ -0,0 +1,13 @@
+package by.salov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson44springsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson44springsecurityApplication.class, args);
+    }
+
+}
---
+++ lesson44springsecurity/src/test/java/by/salov/Lesson44springsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package by.salov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson44springsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
