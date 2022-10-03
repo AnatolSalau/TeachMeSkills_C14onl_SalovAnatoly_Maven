42f4ce4532e995b134092f899425db46451aecba
AnatolSalau <salovanatoly5@mail.ru>
2022-10-03 11:42:26 +0300

    Lesson 44 spring security WebSecurityConfigurerAdapter

---
+++ lesson44springsecurity/src/main/java/by/salov/config/SecutityConfig.java
@@ -0,0 +1,35 @@
+package by.salov.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*Old spring security settings by WebSecurityConfigurerAdapter*/
+@Configuration
+@EnableWebSecurity
+public class SecutityConfig extends WebSecurityConfigurerAdapter {
+    /*configure - object that configure spring security*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /*we may authorize(check) all requests by authorizeHttpRequests*/
+        http.authorizeHttpRequests()
+                /*allow all connection to the users pages*/
+/*                .antMatchers("/user").permitAll()*/
+
+                /*allow access only for method GET*/
+                .mvcMatchers(HttpMethod.GET,"/user/**").permitAll()
+
+                /*allow access all after users*/
+/*                .antMatchers("/users/**").permitAll()*/
+
+                /*disable all connection to the admin pages*/
+                /*.antMatchers("/admin").denyAll()*/
+
+                /*allow access only by authorized*/
+                .antMatchers("/admin").authenticated()
+                .and()
+                .formLogin();
+    }
+}
--- lesson44springsecurity/src/main/java/by/salov/controllers/HomeController.java
+++ lesson44springsecurity/src/main/java/by/salov/controllers/HomeController.java
@@ -4,13 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @PostConstruct
     @GetMapping(path = "/user")
     public String user() {
         return "user.html";
     }
+    @PostConstruct
     @GetMapping(path = "/admin")
     public String admin() {
         return "admin.html";
