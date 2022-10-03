0659f315420e4c40fb16675290a038271548973c
AnatolSalau <salovanatoly5@mail.ru>
2022-10-03 16:24:46 +0300

    Lesson 44 spring security WebSecurityConfigurerAdapter

--- lesson44springsecurity/src/main/java/by/salov/repository/UserRepository.java
+++ lesson44springsecurity/src/main/java/by/salov/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    public User findByLogin(String login);
+     User findByLogin(String login);
 }
