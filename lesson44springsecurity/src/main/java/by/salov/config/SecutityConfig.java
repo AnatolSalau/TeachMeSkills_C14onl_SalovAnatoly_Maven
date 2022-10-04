d14948681132201bbdb7cdd66f081a280d8583e6
AnatolSalau <salovanatoly5@mail.ru>
2022-10-04 12:11:37 +0300

    Lesson 44 spring security WebSecurityConfigurerAdapter

--- lesson44springsecurity/src/main/java/by/salov/config/SecutityConfig.java
+++ lesson44springsecurity/src/main/java/by/salov/config/SecutityConfig.java
@@ -3,16 +3,12 @@ package by.salov.config;
 import by.salov.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 /*Old spring security settings by WebSecurityConfigurerAdapter*/
 @Configuration
