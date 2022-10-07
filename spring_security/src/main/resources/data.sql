deb2f1c1b837c4d2cccf3e3c3701e652710d4c2b
AnatolSalau <salovanatoly5@mail.ru>
2022-10-07 14:24:25 +0300

    Spring Security question 1

---
+++ spring_security/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+insert into my_users(login, position, password, role)
+values('user', 'position1', 'password', 'USER');
+insert into my_users( login, position, password, role)
+values('admin', 'position2', 'password', 'ADMIN');
