30d95688cad6be3f095679cdac5594f3abe4c026
Anatol <98157135+AnatolSalau@users.noreply.github.com>
2022-10-11 13:15:31 +0300

    Update SecurityConfiguration.java

--- lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/congigurations/SecurityConfiguration.java
+++ lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/congigurations/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+/*Нерабочая конфигурация*/
 /*@EnableWebSecurity*/
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
