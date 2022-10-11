194e3617a80769156fc4d5cbc451064711de035d
AnatolSalau <salovanatoly5@mail.ru>
2022-10-11 12:49:33 +0300

    Spring Security Lesson 45 BcryptPasswordEncoder

--- lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/congigurations/CustomAuthenticationProviderSecurityConfiguration.java
+++ lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/congigurations/CustomAuthenticationProviderSecurityConfiguration.java
@@ -1,35 +1,24 @@
 package by.salov.lesson45_spring_security_roles.congigurations;
 
-import by.salov.lesson45_spring_security_roles.services.UserDetailServiceImpl;
+import by.salov.lesson45_spring_security_roles.components.UserAuthencationProviderImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-/*@EnableWebSecurity*/
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+@EnableWebSecurity
+public class CustomAuthenticationProviderSecurityConfiguration  extends WebSecurityConfigurerAdapter {
     @Autowired
-    private UserDetailServiceImpl userDetailService;
+    private UserAuthencationProviderImp userAuthencationProviderImp;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userDetailService)
-                /*Без кодирования все работает*/
-                .passwordEncoder(NoOpPasswordEncoder. getInstance());
-
-                /* C BCryptPasswordEncoder() не работает, но при это я реализовал
-                * AuthenticationProvider - где в ручную прописал сравнение через BCryptPasswordEncoder
-                * там все работает*/
-
-                /* не работает -> */
-                /*.passwordEncoder(new BCryptPasswordEncoder());*/
+                .authenticationProvider(userAuthencationProviderImp);
     }
 
     @Override
--- lesson45_spring_security_roles/src/main/resources/data.sql
+++ lesson45_spring_security_roles/src/main/resources/data.sql
@@ -6,7 +6,7 @@ values (1, 'ADMIN'),
 insert into users (id, login, password)
 values (1, 'admin', 'admin'),
        (2, 'doctor', 'doctor'),
-       (3, 'user', 'user');
+       (3, 'user', '$2a$12$W0Nl7weXLTWsXsz5xt09O.z6i.lViKTBszvhNCZcAKag751vr0duq');
 
 insert into users_roles(user_id,role_id)
 values (1,1),
