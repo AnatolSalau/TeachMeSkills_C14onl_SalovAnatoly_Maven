dcabcb21e506f00184fcf28c3bb9d2424d099ac8
Anatol <98157135+AnatolSalau@users.noreply.github.com>
2022-10-11 13:14:38 +0300

    Update SecurityBeansConfiguration.java

--- lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/congigurations/SecurityBeansConfiguration.java
+++ lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/congigurations/SecurityBeansConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/*Просто создаю бин BCryptPasswordEncoder */
 @Configuration
 public class SecurityBeansConfiguration {
     @Bean
