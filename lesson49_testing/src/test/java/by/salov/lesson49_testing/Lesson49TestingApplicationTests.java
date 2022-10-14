90e8236709a312e44d5c18193b85a9c2639b8844
AnatolSalau <salovanatoly5@mail.ru>
2022-10-14 16:09:02 +0300

    Testing, Logging classwork

---
+++ lesson49_testing/src/test/java/by/salov/lesson49_testing/Lesson49TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.salov.lesson49_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson49TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
