92e795e42ab2546b1a56c94b85017a0f46aae6e0
AnatolSalau <salovanatoly5@mail.ru>
2022-10-14 15:29:03 +0300

    Spring Security Lesson 45 Final AccessSuccessHandler AccessFailureHandler CustomFilter AccessDeniedHandler

--- lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/handlers/CustomAuthenticationFailureHandler.java
+++ lesson45_spring_security_roles/src/main/java/by/salov/lesson45_spring_security_roles/handlers/CustomAuthenticationFailureHandler.java
@@ -2,12 +2,14 @@ package by.salov.lesson45_spring_security_roles.handlers;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
