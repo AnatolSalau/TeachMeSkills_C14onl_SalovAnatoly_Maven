5762f6614bad4c539bda9fa18c8b594749e8c13e
AnatolSalau <salovanatoly5@mail.ru>
2022-10-16 20:28:03 +0300

    Lesson 49 testing JUpiter no spring

--- lesson49_testing/src/test/java/by/salov/lesson49_testing/Lesson49TestingApplicationTests.java
+++ lesson49_testing/src/test/java/by/salov/lesson49_testing/Lesson49TestingApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Lesson49TestingApplicationTests {
 
+
     @Test
     void contextLoads() {
     }
--- lesson49_testing_nospring/src/test/java/services/MathServiceTest.java
+++ lesson49_testing_nospring/src/test/java/services/MathServiceTest.java
@@ -1,9 +1,22 @@
 package services;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
 
 public class MathServiceTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("beforeAll()");
+    }
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("beforeEach()");
+    }
+    @AfterEach
+    public void afterEach() {
+        System.out.println("afterEach()");
+    }
     @Test
     public void testMath() {
         int intOne = 20;
