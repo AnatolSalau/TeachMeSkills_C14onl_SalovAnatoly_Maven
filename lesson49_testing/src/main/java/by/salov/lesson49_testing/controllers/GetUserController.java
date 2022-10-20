de5137981bf17a989d92cacff96f1df487a2e117
AnatolSalau <salovanatoly5@mail.ru>
2022-10-20 19:13:18 +0300

    Lesson 50 assertj library

--- lesson49_testing/src/main/java/by/salov/lesson49_testing/controllers/GetUserController.java
+++ lesson49_testing/src/main/java/by/salov/lesson49_testing/controllers/GetUserController.java
@@ -3,11 +3,9 @@ package by.salov.lesson49_testing.controllers;
 import by.salov.lesson49_testing.domain.User;
 import by.salov.lesson49_testing.exception.UserAllreadyExistExeption;
 import by.salov.lesson49_testing.exception.UserNotExist;
-import by.salov.lesson49_testing.services.UserService;
 import by.salov.lesson49_testing.services.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,14 +24,14 @@ public class GetUserController {
         return userServiceImpl.getAllUsers();
     }
     @GetMapping("/{id}")
-    public User getUserByID(String id) {
+    public User getUserByID(@RequestParam()String id) {
         Long idLong = Long.valueOf(id);
         User userById = userServiceImpl.getUserById(idLong);
         return userById;
     }
 
-    @PostMapping
-    public User user(@RequestBody User user) throws UserAllreadyExistExeption, UserNotExist {
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user) throws UserAllreadyExistExeption, UserNotExist {
         User saveUser = userServiceImpl.saveUser(user);
         return saveUser;
     }
--- lesson49_testing/src/test/java/by/salov/lesson49_testing/controllers/GetUserControllerTest.java
+++ lesson49_testing/src/test/java/by/salov/lesson49_testing/controllers/GetUserControllerTest.java
@@ -1,7 +1,24 @@
 package by.salov.lesson49_testing.controllers;
 
+import by.salov.lesson49_testing.domain.User;
+import by.salov.lesson49_testing.exception.UserAllreadyExistExeption;
+import by.salov.lesson49_testing.exception.UserNotExist;
+import by.salov.lesson49_testing.services.UserService;
+import by.salov.lesson49_testing.services.impl.UserServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,11 +26,59 @@ import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(GetUserController.class)
 class GetUserControllerTest {
 
+    /*Autovired bean UserServiceImpl*/
+    @MockBean
+    private UserServiceImpl serviceImpl;
+
+    /*Create class from Mockito, that will create request instead of user*/
+    @Autowired
+    private MockMvc mockMvc;
+
+    /*ObjectMapper for generating JSON*/
+    @Autowired
+    private ObjectMapper objectMapper;
     @Test
-    void getUserByID() {
+    void testGetUserByID() {
+
     }
 
     @Test
-    void user() {
+    void testAddUser()  {
+        //given
+        User userRequest = User.builder()
+                .login("First").password("Password").build();
+        User userResponse = User.builder()
+                .id(1L).login("First").password("Password").build();
+
+        try {
+            Mockito.when(serviceImpl.saveUser(userRequest)).thenReturn(userResponse);
+        } catch (UserNotExist e) {
+            e.printStackTrace();
+        } catch (UserAllreadyExistExeption e) {
+            e.printStackTrace();
+        }
+        //when
+        /*Create request by MockMVC*/
+        ResultActions resultActions = null;
+        try {
+            resultActions = mockMvc.perform(MockMvcRequestBuilders
+                    .post("/")
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(userRequest))
+            ).andDo(MockMvcResultHandlers.print());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //then
+        try {
+            resultActions
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath(
+                            "$.id", CoreMatchers.is(1L)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
