96def3b3e4c7c0c9b5c545ee71ed7fa5e2ef0224
AnatolSalau <salovanatoly5@mail.ru>
2022-10-23 00:11:23 +0300

    Integrations Tests

--- lesson49_testing/src/test/java/by/salov/lesson49_testing/repository/UserRepositoryIntegrationTest.java
+++ lesson49_testing/src/test/java/by/salov/lesson49_testing/repository/UserRepositoryIntegrationTest.java
@@ -1,8 +1,34 @@
 package by.salov.lesson49_testing.repository;
 
+import by.salov.lesson49_testing.domain.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class UserRepositoryIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void beforeEach() {
+
+    }
+
+    @Test
+    public void testGetByID() {
+        User byId = userRepository.getById(7L);
+/*        System.out.println(byId);*/
+
+    }
 }
