4da8577f098b43be09bbcb7ec1f3cb16aed2180c
AnatolSalau <salovanatoly5@mail.ru>
2022-10-26 18:12:51 +0300

    Integrations Tests

--- lesson49_testing/src/test/java/by/salov/lesson49_testing/controllers/UserExternalResourceIntegrationTest.java
+++ lesson49_testing/src/test/java/by/salov/lesson49_testing/controllers/UserExternalResourceIntegrationTest.java
@@ -3,9 +3,12 @@ package by.salov.lesson49_testing.controllers;
 import by.salov.lesson49_testing.domain.User;
 import by.salov.lesson49_testing.services.impl.UserServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,18 +29,36 @@ public class UserExternalResourceIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    //Create external resource by WireMock and register like extension in JUnit5
+    //put port from external resource where feign client get data
+    @RegisterExtension
+    static WireMockExtension WIREMOCKEX = WireMockExtension.newInstance()
+            .options(WireMockConfiguration.options().port(8099))
+            .build();
+    //
     @Test
     void  testAddUser() throws Exception {
         //given
-        User userRequest = User.builder()
-                .login("First").password("Password").build();
-        User userResponse = User.builder()
-                .id(1L).login("First").password("Password").build();
+        User userRequestExternalResource = User.builder()
+                .login("ExternalLogin1").password("ExternalPassword1").build();
+        User userResponseExternalResource = User.builder()
+                .id(1L).login("ExternalLogin1").password("ExternalPassword1").build();
 
+        String userResponseString = objectMapper.writeValueAsString(userResponseExternalResource);
+
+        //knocking wiremock by the url where feign client get data
+        //@FeignClient(name = "UserFeignClient", url = "http://127.0.0.1:8099", path = "/user")
+        //public interface UserFeignClient {}
+        WIREMOCKEX.stubFor(WireMock.get("/user")
+                .willReturn(WireMock
+                        .ok(userResponseString)
+                        .withHeader("Content-Type", "application/json")
+                )
+        );
 
         //when
         /*Create request by MockMVC*/
-        String jsonUserRequest = objectMapper.writeValueAsString(userRequest);
+        String jsonUserRequest = objectMapper.writeValueAsString(userRequestExternalResource);
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
                 .post("/test")
                 .contentType("application/json")
@@ -49,6 +70,11 @@ public class UserExternalResourceIntegrationTest {
         resultActions
                 .andExpect(MockMvcResultMatchers.status().is(200))
                 .andExpect(MockMvcResultMatchers.jsonPath(
-                        "$.id", CoreMatchers.is(1)));
+                        "$.id", CoreMatchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath(
+                        "$.login", CoreMatchers.is("ExternalLogin1")))
+                .andExpect(MockMvcResultMatchers.jsonPath(
+                        "$.password", CoreMatchers.is("ExternalPassword1")))
+        ;
     }
 }
