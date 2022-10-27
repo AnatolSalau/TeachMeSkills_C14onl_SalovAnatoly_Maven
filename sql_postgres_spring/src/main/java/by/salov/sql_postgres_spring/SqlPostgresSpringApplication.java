4ee4fd40ca951e534c253a682c50d4d256d998d5
AnatolSalau <salovanatoly5@mail.ru>
2022-10-27 19:47:16 +0300

    sql_postgres initial commit

---
+++ sql_postgres_spring/src/main/java/by/salov/sql_postgres_spring/SqlPostgresSpringApplication.java
@@ -0,0 +1,13 @@
+package by.salov.sql_postgres_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPostgresSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlPostgresSpringApplication.class, args);
+    }
+
+}
---
+++ sql_postgres_spring/src/test/java/by/salov/sql_postgres_spring/SqlPostgresSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package by.salov.sql_postgres_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPostgresSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
