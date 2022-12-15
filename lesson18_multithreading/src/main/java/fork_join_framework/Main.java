d968ed8befde084f4345a88760527961cd31f2a5
AnatolSalau <salovanatoly5@mail.ru>
2022-12-15 16:19:55 +0300

    CompletableFeature, Executors in Autoclosable, ThreadFactory

---
+++ lesson18_multithreading/src/main/java/fork_join_framework/Main.java
@@ -0,0 +1,11 @@
+package fork_join_framework;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.debug("App is started! DEBUG");
+        log.info("App is started! INFO");
+    }
+}
