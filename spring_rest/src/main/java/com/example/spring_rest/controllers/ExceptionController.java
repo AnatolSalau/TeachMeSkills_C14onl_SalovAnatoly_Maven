d0b132a7e48d72d45f11e4f28ca1b3b91c02088d
AnatolSalau <salovanatoly5@mail.ru>
2023-01-13 15:57:02 +0300

    Spring Exception

--- spring_rest/src/main/java/com/example/spring_rest/controllers/ExceptionController.java
+++ spring_rest/src/main/java/com/example/spring_rest/controllers/ExceptionController.java
@@ -1,9 +1,7 @@
 package com.example.spring_rest.controllers;
 
-import com.example.spring_rest.dto.ErrorDTO;
 import com.example.spring_rest.services.ExceptionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- spring_rest/src/main/java/com/example/spring_rest/dto/ErrorDTO.java
+++ spring_rest/src/main/java/com/example/spring_rest/dto/ErrorDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Just object, which RestAdviceController will response to client
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
