940875b5def9be67b59ba08e1f3809836802d600
AnatolSalau <salovanatoly5@mail.ru>
2023-01-16 16:02:35 +0300

    ExceptionHandler

--- rest_microservice_userbackend/src/main/java/com/example/rest_microservice_userbackend/handler/MyExceptionHandler.java
+++ rest_microservice_userbackend/src/main/java/com/example/rest_microservice_userbackend/handler/MyExceptionHandler.java
@@ -16,16 +16,20 @@ import java.util.List;
 public class MyExceptionHandler {
 
     @ExceptionHandler(value
-            = {RuntimeException.class, UserRuntimeException.class})
-    public ResponseEntity<ErrorDTO> exceptRuntimeException(
-            RuntimeException exception, UserRuntimeException userRuntimeException) {
+            = {RuntimeException.class})
+    public ResponseEntity<ErrorDTO> exceptRuntimeException(RuntimeException exception) {
         List<String> messages = new ArrayList<>();
-        if (exception != null) {
-            messages.add(exception.getMessage());
-        }
-        if (userRuntimeException != null) {
-            messages.add(userRuntimeException.getMessage());
-        }
+        messages.add(exception.getMessage());
+        return ResponseEntity
+                . status(400)
+                .body(new ErrorDTO(messages));
+    }
+
+    @ExceptionHandler(value
+            = {UserRuntimeException.class })
+    public ResponseEntity<ErrorDTO> exceptUserRuntimeException(UserRuntimeException exception) {
+        List<String> messages = new ArrayList<>();
+        messages.add(exception.getMessage());
         return ResponseEntity
                 . status(400)
                 .body(new ErrorDTO(messages));
