94381693df98a28d18945044a4ee833f90b72661
AnatolSalau <salovanatoly5@mail.ru>
2023-01-16 10:45:19 +0300

    Userbackend

---
+++ rest_microservice_userbackend/src/main/java/com/example/rest_microservice_userbackend/RestMicroserviceUserbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_microservice_userbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMicroserviceUserbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMicroserviceUserbackendApplication.class, args);
+    }
+
+}
---
+++ rest_microservice_userbackend/src/test/java/com/example/rest_microservice_userbackend/RestMicroserviceUserbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_microservice_userbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMicroserviceUserbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
