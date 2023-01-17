e2b508b3f6f079320f2c767f37e326c7ba058fb5
AnatolSalau <salovanatoly5@mail.ru>
2023-01-17 15:01:27 +0300

    Exception handle in microservice architect

--- rest_microservice_facade/src/main/java/com/example/rest_microservice_facade/configurations/MicroserviceConfig.java
+++ rest_microservice_facade/src/main/java/com/example/rest_microservice_facade/configurations/MicroserviceConfig.java
@@ -1,6 +1,8 @@
 package com.example.rest_microservice_facade.configurations;
 
+import com.example.rest_microservice_facade.handlers.RestTemplateErrorHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +15,11 @@ public class MicroserviceConfig {
 
     @Bean
     RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        RestTemplate build = restTemplateBuilder
+                .errorHandler(new RestTemplateErrorHandler())
+                .build();
+        return build;
     }
 
     @Bean
--- rest_microservice_facade/src/main/java/com/example/rest_microservice_facade/exception/UserRuntimeException.java
+++ rest_microservice_facade/src/main/java/com/example/rest_microservice_facade/exception/UserRuntimeException.java
@@ -1,5 +1,8 @@
 package com.example.rest_microservice_facade.exception;
 
+import lombok.Getter;
+
+@Getter
 public class UserRuntimeException extends RuntimeException{
 
     private int statusCode;
