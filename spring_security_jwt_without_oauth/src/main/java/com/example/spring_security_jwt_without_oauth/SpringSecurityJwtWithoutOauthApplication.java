a0bc43d0deabfb2e3144a6f06de69fa82264fb0d
AnatolSalau <salovanatoly5@mail.ru>
2023-01-18 20:46:09 +0300

    init commit

---
+++ spring_security_jwt_without_oauth/src/main/java/com/example/spring_security_jwt_without_oauth/SpringSecurityJwtWithoutOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_jwt_without_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtWithoutOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtWithoutOauthApplication.class, args);
+	}
+
+}
---
+++ spring_security_jwt_without_oauth/src/test/java/com/example/spring_security_jwt_without_oauth/SpringSecurityJwtWithoutOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_jwt_without_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtWithoutOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
