1a89b179342006cc8624d9eb4096c27865b7ab0c
AnatolSalau <salovanatoly5@mail.ru>
2023-01-18 21:15:37 +0300

    init commit

--- spring_security_jwt_without_oauth/src/main/resources/db/migration/V1__init_db_schema.sql
+++ spring_security_jwt_without_oauth/src/main/resources/db/migration/V1__init_db_schema.sql
@@ -2,20 +2,29 @@ create table if not exists roles
 ( id bigint not null primary key,
   name varchar (255) not null
 );
+
+CREATE SEQUENCE IF NOT EXISTS roles_id_seq
+start 1
+increment 1;
+
 create table if not exists users
 (   id bigint not null primary key,
     login varchar (255) not null,
     password varchar(255) not null
 );
 
+CREATE SEQUENCE IF NOT EXISTS users_id_seq
+start 1
+increment 1;
+
 insert into roles (id, name)
-values (1, 'ROLE_ADMIN'),
-       (2, 'ROLE_DOCTOR'),
-       (3, 'ROLE_USER');
+values (-1, 'ROLE_ADMIN'),
+       (-2, 'ROLE_DOCTOR'),
+       (-3, 'ROLE_USER');
 insert into users (id, login, password)
-values (1, 'admin',
+values (-1, 'admin',
         '$2a$12$MWSgG3DAUk5zmWCKuZ6ohuItYk4z73t4wpu.m9yIC2JiTum0xY9bK'),
-       (2, 'doctor',
+       (-2, 'doctor',
         '$2a$12$t3btj.mOyPWOlSsxvG4Ajev/y9nDunjwUa7Pp1CnIkDHN41ZwGbdW'),
-       (3, 'user',
+       (-3, 'user',
         '$2a$12$14MAJqLY1kmV7aLpaXaHwu1ILzl5hQHRuX6M5IA2dzB.PGQRmLwH6');
