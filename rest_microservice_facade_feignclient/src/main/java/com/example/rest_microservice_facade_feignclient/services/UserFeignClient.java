3d57a1868d9fa11154d5de2de9174d87ea0af4de
AnatolSalau <salovanatoly5@mail.ru>
2023-01-18 13:59:42 +0300

    OpenFeign userbackend  microservice

--- rest_microservice_facade_feignclient/src/main/java/com/example/rest_microservice_facade_feignclient/services/UserFeignClient.java
+++ rest_microservice_facade_feignclient/src/main/java/com/example/rest_microservice_facade_feignclient/services/UserFeignClient.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * OpenFeignClient for connect with User microservice
+ *  OpenFeignClient to connect with User microservice
  */
 @FeignClient(
         name = "user-feign",
