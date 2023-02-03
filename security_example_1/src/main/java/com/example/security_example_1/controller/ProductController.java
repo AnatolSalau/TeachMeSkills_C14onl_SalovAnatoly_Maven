cf103468e4885b825763208021b1068d6d20be5e
AnatolSalau <salovanatoly5@mail.ru>
2023-02-03 11:20:19 +0300

    Spring security 3.0

---
+++ security_example_1/src/main/java/com/example/security_example_1/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.example.security_example_1.controller;
+
+import com.example.security_example_1.dto.Product;
+import com.example.security_example_1.entity.UserInfo;
+import com.example.security_example_1.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo){
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+}
---
+++ security_example_1/src/test/java/com/example/security_example_1/SecurityExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_example_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
