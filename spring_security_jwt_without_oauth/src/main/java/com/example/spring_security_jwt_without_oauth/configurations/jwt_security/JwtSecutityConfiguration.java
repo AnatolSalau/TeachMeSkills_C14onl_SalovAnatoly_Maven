638920988b5f2b577fe4a4a94d9d43fa39f28eaf
AnatolSalau <salovanatoly5@mail.ru>
2023-02-04 12:10:08 +0300

    init commit

--- spring_security_jwt_without_oauth/src/main/java/com/example/spring_security_jwt_without_oauth/configurations/jwt_security/JwtSecutityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring_security_jwt_without_oauth.configurations.jwt_security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-
-public class JwtSecutityConfiguration {
-
-}
