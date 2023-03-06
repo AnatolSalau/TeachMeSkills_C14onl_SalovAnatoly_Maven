b41468f28874a21a648c5ea0b92cf321bc59ac48
AnatolSalau <salovanatoly5@mail.ru>
2023-03-06 21:41:34 +0300

    init commit initialize DB by FlyWay migration

---
+++ spring_security_acl/src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,29 @@
+CREATE SEQUENCE IF NOT EXISTS roles_id_seq
+    start 1
+    increment 1;
+CREATE SEQUENCE IF NOT EXISTS users_id_seq
+    start 1
+    increment 1;
+create table if not exists users
+(
+    id       bigint              not null primary key,
+    login    varchar(255) unique not null,
+    password varchar(255)        not null
+);
+create table if not exists roles
+(
+    id      bigint       not null primary key,
+    role    varchar(255) not null,
+    user_id bigint references users
+);
+insert into users(id, login, password)
+values (-1, 'user',
+        '$2a$12$6BlCjcTXmS/3IfcSiNI2COpDKk8LEqKr7PXcv9GJuLWgJfIXhGvRC'),
+       ( -2, 'doctor'
+       , '$2a$12$Hrm75q6NsauybkERr3Kz9eKX1JzIm2FaGMpRmg9t7HizfnPoqVG/2'),
+       (-3, 'admin',
+        '$2a$12$6aZWlxR/7WmzEobBqezqh.uYXjXdV0VCpO2FZVRXB/Pl9gD5D.qiC');
+insert into roles(id, role)
+values (-1, 'USER'),
+       (-2, 'DOCTOR'),
+       (-3, 'ADMIN');
