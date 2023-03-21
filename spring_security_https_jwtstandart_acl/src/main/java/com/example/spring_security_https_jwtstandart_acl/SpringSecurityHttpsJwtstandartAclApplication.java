3d506ff1987bb846ddb1117bc7e581bbed87c790
AnatolSalau <salovanatoly5@mail.ru>
2023-03-21 09:19:42 +0300

    FlyDB

---
+++ spring_security_https_jwtstandart_acl/src/main/java/com/example/spring_security_https_jwtstandart_acl/SpringSecurityHttpsJwtstandartAclApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_https_jwtstandart_acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHttpsJwtstandartAclApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SpringSecurityHttpsJwtstandartAclApplication.class, args);
+      }
+
+}
---
+++ spring_security_https_jwtstandart_acl/src/test/java/com/example/spring_security_https_jwtstandart_acl/SpringSecurityHttpsJwtstandartAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_https_jwtstandart_acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHttpsJwtstandartAclApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
